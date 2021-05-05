719b681264098ee57b77c34085ac848f21fbded4
EneruJ <wassime.mezzache@universite-paris-saclay.fr>
2021-05-05 12:13:02 +0200

    Première version (Inscription/Connexion)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class HomeController extends AbstractController
+{
+
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
